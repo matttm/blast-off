4935a25478cd5a5926fbaf066ddfe4142acddc6f
matttm <matttmaloney@gmail.com>
2021-02-27 10:06:31 -0500

    setting up routing

--- blast-off-frontend/src/app/app.module.ts
+++ blast-off-frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
