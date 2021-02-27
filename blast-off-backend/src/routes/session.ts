2ad64342fc093a615117c3714f5b17f0c9fa1c7a
matttm <matttmaloney@gmail.com>
2021-02-27 09:06:25 -0500

    added user validation and loggout session delete

--- blast-off-backend/src/routes/session.ts
+++ blast-off-backend/src/routes/session.ts
@@ -1,13 +1,15 @@
 import express, {json} from 'express';
 import * as jwt from 'jsonwebtoken';
+import {getConnection} from "typeorm";
 
 const router = express.Router();
 const RSA_PRIVATE_KEY = process.env.RSA_PRIVATE_KEY || 'secret';
 const EXPIRES_IN = process.env.EXPIRES_IN || 126;
+const SESSION_COOKIE_KEY = "SESSIONID";
 
 /* GET home page. */
 router.route('/')
-    .post((req, res) => {
+    .post(async (req, res) => {
         const {
             username,
             firstName,
@@ -16,6 +18,10 @@ router.route('/')
             role
         } = req.body;
         // TODO: validate user
+        if (! await isValidUser(username, password)) {
+            res.status(401).send();
+            return;
+        }
         // TODO: get userId
         const userId = 0;
         const payload = {
@@ -29,18 +35,23 @@ router.route('/')
             subject: userId
         });
         // set it in an HTTP Only + Secure Cookie
-        res.cookie("SESSIONID", jwtBearerToken,
+        res.cookie(SESSION_COOKIE_KEY, jwtBearerToken,
             {
                 httpOnly: true,
                 secure: true,
                 expires: new Date(Date.now() + (EXPIRES_IN as number))
-        })
+            })
             .status(201)
             .json();
     })
     .delete((req, res) => {
-        // TODO: implement
-        res.status(403);
+        res.clearCookie(SESSION_COOKIE_KEY).status(200);
     });
+
+async function isValidUser(username: string, password: string): Promise<boolean> {
+    const userRepo = await getConnection().getRepository('User');
+    const user = await userRepo.find({ where: {username, password}});
+    return !!user;
+}
 
 export default router;
