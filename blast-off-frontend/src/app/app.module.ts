4ff38dfa185e81c5dfb34d6186aec9e9f61c44f9
matttm <matttmaloney@gmail.com>
2021-02-27 15:16:46 -0500

    added login component

--- blast-off-frontend/src/app/app.module.ts
+++ blast-off-frontend/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './components/app/app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { LoginComponent } from './components/login/login.component';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,8 +17,13 @@ import { LoginComponent } from './components/login/login.component';
     LoginComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
     MatToolbarModule
   ],
   providers: [],
--- blast-off-frontend/src/app/components/login/login.component.ts
+++ blast-off-frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+interface LoginForm {
+  username: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  model: LoginForm;
 
-  constructor() { }
+  constructor() {
+    this.model = {password: '', username: ''};
+  }
 
   ngOnInit(): void {
   }
+
+  onSubmit(): void {}
 
 }
