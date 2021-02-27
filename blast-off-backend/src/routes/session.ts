879f85caaa331464bb80629a35ffb38c91cc835f
matttm <matttmaloney@gmail.com>
2021-02-26 20:50:49 -0500

    working on jwt

--- blast-off-backend/src/routes/session.ts
+++ blast-off-backend/src/routes/session.ts
@@ -1,11 +1,42 @@
-import express from 'express';
+import express, {json} from 'express';
+import * as jwt from 'jsonwebtoken';
+
 const router = express.Router();
+const RSA_PRIVATE_KEY = process.env.RSA_PRIVATE_KEY || 'secret';
+const EXPIRES_IN = process.env.EXPIRES_IN || 126;
 
 /* GET home page. */
 router.route('/')
     .post((req, res) => {
-        // TODO: implement
-        res.status(403);
+        const {
+            username,
+            firstName,
+            lastName,
+            password,
+            role
+        } = req.body;
+        // TODO: validate user
+        // TODO: get userId
+        const userId = 0;
+        const payload = {
+            userId,
+            username,
+            role
+        };
+        const jwtBearerToken = jwt.sign(payload, RSA_PRIVATE_KEY, {
+            algorithm: 'RS256',
+            expiresIn: EXPIRES_IN,
+            subject: userId
+        });
+        // set it in an HTTP Only + Secure Cookie
+        res.cookie("SESSIONID", jwtBearerToken,
+            {
+                httpOnly: true,
+                secure: true,
+                expires: new Date(Date.now() + (EXPIRES_IN as number))
+        })
+            .status(201)
+            .json();
     })
     .delete((req, res) => {
         // TODO: implement
